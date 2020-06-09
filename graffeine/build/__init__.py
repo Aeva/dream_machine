de932a13a10b2cf17314915f5844a85ca811c01e
Aeva PaleceK <aeva.ntsc@gmail.com>
2020-06-09 17:37:11 -0500

    building on windows

--- graffeine/build/__init__.py
+++ graffeine/build/__init__.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import shutil
 import subprocess
 from glob import glob
 
@@ -17,19 +18,31 @@ def find_exe(name):
     raise RuntimeError(f"Can't find {name}.  Is it in your PATH env var?")
 
 
-def build(source_path, out_path="a.out"):
+def build(source_path, out_path="a.out", copy_dlls=True):
     cc = find_exe("clang++")
+    defines = ["GLFW_DLL"]
     includes = glob(os.path.join(os.path.dirname(__file__), "**", "include"))
     libs = glob(os.path.join(os.path.dirname(__file__), "**", "lib"))
     sources = [source_path]
     sources += glob(os.path.join(os.path.dirname(__file__), "**", "src", "*.cpp"))
     sources += glob(os.path.join(os.path.dirname(__file__), "**", "src", "*.c"))
+    dlls = ["glfw3.dll"]
 
     args = [cc]
+    args += [f"-D{p}" for p in defines]
     args += [f"-I{p}" for p in includes]
     args += [f"-L{p}" for p in libs]
-    args += ["-lglfw3", "-lopengl32"]
+    args += ["-lglfw3dll", "-lopengl32"]
     args += sources
 
     print(" ".join(args))
     print(subprocess.run(args, check=True))
+
+    if copy_dlls:
+        for path in libs:
+            for dll in dlls:
+                src = os.path.join(path, dll)
+                if os.path.isfile(src):
+                    dst = os.path.join(os.getcwd(), dll)
+                    if not os.path.isfile(dst):
+                        shutil.copyfile(src, dst)
